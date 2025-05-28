bdd85d3e9ddf60996eb8362fd5b7ce49ad0e8b12
Qing-Yun-Dev <18359185464@163.com>
2025-05-28 18:18:27 +0800

    更新 Test.lua

--- Test.lua
+++ Test.lua
@@ -4,5 +4,6 @@ return {
     [3] = {"b549299c-8c7d-4eb3-92df-bdcd2ca110ac", false},
     [4] = {"02e26298-266a-40d8-b559-c940e3806c16", false},
     [5] = {"b07b21a9-91d3-464a-adda-399d9c3582f8", false},
-    [6] = {"60bbdabc-d90a-4dd1-9cba-52c99bca0d17", false}
+    [6] = {"60bbdabc-d90a-4dd1-9cba-52c99bca0d17", false},
+    [7] = {"fb42295c-f206-4bdc-82d9-54e9cd9fdae3", false}
 }

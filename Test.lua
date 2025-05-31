4dfc6b7ac06671efd03c0810f98913f60fd3429b
Qing-Yun-Dev <18359185464@163.com>
2025-06-01 01:50:06 +0800

    更新 Test.lua

--- Test.lua
+++ Test.lua
@@ -5,5 +5,6 @@ return {
     [4] = {"02e26298-266a-40d8-b559-c940e3806c16", false},
     [5] = {"b07b21a9-91d3-464a-adda-399d9c3582f8", false},
     [6] = {"60bbdabc-d90a-4dd1-9cba-52c99bca0d17", false},
-    [7] = {"415ac69c-c746-4481-a533-ba3eaf5d0f47", false}
+    [7] = {"415ac69c-c746-4481-a533-ba3eaf5d0f47", false},
+    [1] = {"9a70d7be-e5de-434e-9145-3c480b851437", false}
 }

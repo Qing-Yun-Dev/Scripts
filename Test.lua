f3dbc3ab7cb1832e7b1adaee0004b946e032c67c
Qing-Yun-Dev <18359185464@163.com>
2025-06-24 20:07:48 +0800

    更新 Test.lua

--- Test.lua
+++ Test.lua
@@ -25,5 +25,6 @@ return {
     [24] = {"8acb1456-4a86-4cb8-bdd2-105b8a76d916", false},
     [25] = {"60bbdabc-d90a-4dd1-9cba-52c99bca0d17", false},
     [26] = {"41c8b07d-93da-4a27-bd1a-934de2fa38a7", false},
-    [27] = {"4504bd17-3398-4b70-ba08-80cadb8c380a", false}
+    [27] = {"4504bd17-3398-4b70-ba08-80cadb8c380a", false},
+    [28] = {"8918f764-46fa-45d3-b948-eb123375ac2b", false}
 }

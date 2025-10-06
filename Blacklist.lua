c793c2491ee863a1aee641a7acc12c27ee85d44b
Qing-Yun-Dev <18359185464@163.com>
2025-10-07 03:30:52 +0800

    创建 Blacklist.lua

---
+++ Blacklist.lua
@@ -0,0 +1,14 @@
+return {
+    [1] = {"AQ14ABS", true, "LOL"},
+    [2] = {"edc12363", true, "LOL"},
+    [3] = {"onygfvjop", true, "LOL"},
+    [4] = {"ah_kdj3", true, "LOL"},
+    [5] = {"luoyang100616", true, "LOL"},
+    [6] = {"hdjdje675", true, "LOL"},
+    [7] = {"qazwsxuruu", true, "瞎骂"},
+    [8] = {"ADCZ4xx", true, "LOL"},
+    [9] = {"hjcjb96", true, "LOL"},
+    [10] = {"TM5418888", true, "牢营长"},
+    [11] = {"codm656558", true, "LOL"},
+    [12] = {"guajichina", true, "Httadmin"}
+}

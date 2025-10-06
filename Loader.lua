2f9553c93a66136716961ad8d3a90c9bebddae82
Qing-Yun-Dev <18359185464@163.com>
2025-10-07 04:22:29 +0800

    更新 Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,4 +2,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Qing-Yun-Dev/Scripts/
     [2091953627] = "Rooms%20%26%20Doors",
     [4367208330] = "Pressure",
     [6331902150] = "Forsaken"
-})[game.GameId] or "ScriptsHub")))()
+})[game.GameId] or "Script%20Hub")))()

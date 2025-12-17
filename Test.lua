2c43391d5a3e22ffd9aecf8ff1783effd4b597c7
Qing-Yun-Dev <18359185464@163.com>
2025-12-17 21:38:04 +0800

    更新 Test.lua

--- Test.lua
+++ Test.lua
@@ -50,5 +50,7 @@ return {
     [49] = {"2cada1fc-f5b3-43e7-9434-f6f423094e7b", false},
     [50] = {"84c9629f-f639-4a67-8007-83126c447ae7", false},
     [51] = {"2507d4a8-1d02-4f49-8ab4-222464072984", false},
-    [52] = {"025dcb0e-c00e-4be8-8fd1-462d12287a2c", false}
+    [52] = {"025dcb0e-c00e-4be8-8fd1-462d12287a2c", false},
+    [53] = {"ef9ec89b-95d5-4db9-8faa-74311fd46a32", false},
+    [54] = {"825790AA-6F03-45AF-B33F-5651B16962EC", false}
 }

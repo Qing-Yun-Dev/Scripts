6e4252c3692455381eeb4225b3ef39b2ab82358f
Qing-Yun-Dev <18359185464@163.com>
2026-01-01 18:04:53 +0800

    更新 Test.lua

--- Test.lua
+++ Test.lua
@@ -54,5 +54,6 @@ return {
     [53] = {"ef9ec89b-95d5-4db9-8faa-74311fd46a32", false},
     [54] = {"825790AA-6F03-45AF-B33F-5651B16962EC", false},
     [55] = {"349CBCC7-FBC0-4240-A877-686FC9E8C41A", false},
-    [56] = {"56e05958-fd32-404e-aec2-57d9c60bbfba", false}
+    [56] = {"56e05958-fd32-404e-aec2-57d9c60bbfba", false},
+    [57] = {"3c46ba80-41fc-495e-8576-3ed100854b84", false}
 }

b0a149072814518623c4719758d1d7c9daf0685a
Qing-Yun-Dev <3828086635@qq.com>
2026-04-06 02:48:11 +0800

    更新 Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Qing-Yun-Dev/Scripts/Main/"..(({
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Qing-Yun-Dev/Scripts/Main/" .. (({
     [12208518151] = "Rooms%20%26%20Doors",
     [13040045689] = "Rooms%20%26%20Doors",
     [12552538292] = "Pressure",
     [124505452456262] = "Pressure",
     [12552511828] = "Pressure"
-})[game.PlaceId] or "Script%20Hub")..".lua"))()
+})[game.PlaceId] or "Script%20Hub") .. ".lua"))()
